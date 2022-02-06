63bb91c61889ff2b392cf43fb3e37c60d6a6e81b
samarthtrex <70048740+samarthtrex@users.noreply.github.com>
2022-02-06 14:26:57 +0530

    Add files via upload

---
+++ pro 106/project .py
@@ -0,0 +1,24 @@
+import csv
+import numpy as np
+
+def getDataSource(data_path):
+    marks_in_percentage= []
+    days_present= []
+    with open(data_path) as csv_file:
+        csv_reader=csv.DictReader(csv_file)
+        for row in csv_reader:
+            marks_in_percentage.append(float(row['Roll No']))
+            days_present.append(float(row['Marks In Percentage']))
+
+    return {'x': marks_in_percentage,'y': days_present}
+
+def findCorrelation(datasource):
+    correlation=np.corrcoef(datasource['x'],datasource['y'])  
+    print(correlation[0,1])  
+
+def setup():
+    data_path='data.csv'
+    datasource= getDataSource(data_path)
+    findCorrelation(datasource)
+    
+setup()        
